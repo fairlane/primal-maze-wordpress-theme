7efdf4c4b4f4e0c06367a0e398b61bf075b9fcd6
Hannu Pölönen <hannu.polonen@nosto.com>
2018-12-01 17:16:33 +0200

    Some CSS tunings

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
+define('PM_SNIPPETS_DIR', __DIR__  . '/snippets/');
+
 function primalmaze_enqueue_styles() {
 
-    $parent_style = 'twentyseventeen-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'simple-bootstrap-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
---
+++ snippets/header-logo.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="col-lg-12 pm-header-logo">
+        <h1>WASSUP BOIS?</h1>
+    </div>
+</div>
