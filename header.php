9fe15711116df013b47b8e306324f6fea38f6527
Hannu Pölönen <hannu@buukkaa-bandi.fi>
2018-12-06 18:08:25 +0200

    Add font & continue styling

--- header.php
+++ header.php
@@ -13,6 +13,7 @@ $faviconUrl = sprintf('%s/img/favicon', get_stylesheet_directory_uri());
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $faviconUrl; ?>/favicon-16x16.png">
     <link rel="manifest" href="<?php echo $faviconUrl; ?>/site.webmanifest">
     <link rel="mask-icon" href="<?php echo $faviconUrl; ?>/safari-pinned-tab.svg" color="#5bbad5">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 	<?php wp_head(); ?>
@@ -27,7 +28,7 @@ else {
 }
 ?>
 
-<body <?php body_class(); echo $bgCss ?> >
+<body <?php body_class(); ?> >
 
 	<div id="content-wrapper">
 
@@ -76,5 +77,5 @@ else {
         </div>
         <?php endif ?>
 
-		<div id="page-content">
+		<div id="page-content" <?php echo $bgCss; ?>>
 			<div class="container pm-main-content">
