7848522d7efc0c6ef2a5fef091674510e72bc46f
Hannu <hannu@buukkaa-bandi.fi>
2018-12-12 23:07:18 +0200

    Decrease the font size & make the header a bit shorter

--- functions.php
+++ functions.php
@@ -1,18 +1,17 @@
 <?php
 define('PM_SNIPPETS_DIR', __DIR__  . '/snippets/');
 define('PM_ICONS_DIR_RELATIVE', '/img/social/');
+define('PM_PARENT_THEME_STYLE', 'wpbs-style');
 
 /**
  * Enqueues custom styles for the theme
  */
 function primalmaze_enqueue_styles() {
 
-    $parent_style = 'simple-bootstrap-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( PM_PARENT_THEME_STYLE, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+        array(PM_PARENT_THEME_STYLE),
         wp_get_theme()->get('Version')
     );
 }
